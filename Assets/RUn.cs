f2b3e87c7f570ea4352c94ff9ed93df0f255b735
Rok <56684849+openroomxyz@users.noreply.github.com>
2020-03-28 18:15:40 +0100

    ok

---
+++ Assets/RUn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RUn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = 3047;
+        int sum = 0;
+        for(int i = 0; i < num; i++)
+        {
+            if(((i % 5)==0) || ((i % 3) == 0))
+            {
+                sum += i;
+            }
+        }
+        Debug.Log(sum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
